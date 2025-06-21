239dbf229dd568d03635f490d362e8532f05038c
Everton Cruz <cruz.everton@gmail.com>
2025-06-21 10:24:37 -0300

    firebase initialize once

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
 const serviceAccount = require ("../../firebase-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+}
 
 export default admin;
+
