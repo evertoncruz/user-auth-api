03f4cd7bb3ca253f06217307355e5dc86065bdb2
Everton Cruz <cruz.everton@gmail.com>
2025-06-21 07:37:31 -0300

    fix firebase setup

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../firebase-service-account.json";
+const serviceAccount = require ("../../firebase-service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
